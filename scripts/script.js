869c84bcf96b7d4d3a9aa882bf9e25890bd59314
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-10-20 18:48:04 +0200

    Dynamic data

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,45 @@
 var div = React.DOM.div;
 var font_size = 3;
 var progress = 20;
-var data = courses_data[0];
+var index = 0;
+var data = courses_data[index];
+var dataLength = courses_data.length;
 
-var course = function() {
-    return div({id:'kurs', className:'media course'}, 
-                div({className:'media-left'},
-                    React.createElement('img', {className:'media-object', alt:'Miniatura Kursu', src:data.image})
-                ),
-                div({className:'media-body'},
-                    React.createElement('h3', {className:'media-heading', style:{fontSize:font_size + 'em'}}, data.title), 
-                    React.createElement('p', {style:{fontSize:(font_size/2) + 'em'}}, data.description), 'Ładowanie...',
-                    div({className:'progress'},
-                        div({className:'progress-bar progress-bar-success progress-bar-striped active', style:{width:progress + '%'}},
-                            React.createElement('span', {}, progress + ' %')
-                        )
-                    )
-                )
-            );
+var course = function (data) {
+    return div({ id: 'kurs', className: 'media course' },
+        div({ className: 'media-left' },
+            React.createElement('img', { className: 'media-object', alt: 'Miniatura Kursu', src: data.image })
+        ),
+        div({ className: 'media-body' },
+            React.createElement('h3', { className: 'media-heading', style: { fontSize: font_size + 'em' } }, data.title),
+            React.createElement('p', { style: { fontSize: (font_size / 2) + 'em' } }, data.description)
+        )
+    );
 }
 
-document.getElementById('grow').addEventListener('click', function() {
+document.getElementById('grow').addEventListener('click', function () {
     font_size = font_size + 1;
     render();
 });
 
-document.getElementById('shrink').addEventListener('click', function() {
+document.getElementById('shrink').addEventListener('click', function () {
     font_size = font_size === 1 ? font_size : (font_size - 1);
     render();
 });
 
-var interval;
-document.getElementById('start').addEventListener('click', function() {
-    interval = setInterval(function() {
-        progress = progress + 1;
-        render();
-    }, 300);
-});
-
-document.getElementById('stop').addEventListener('click', function() {
-    clearInterval(interval);
+document.getElementById('prev').addEventListener('click', function () {
+    index = index === 0 ? 0 : index - 1;  
     render();
 });
 
-document.getElementById('reset').addEventListener('click', function() {
-    progress = 0;
-    clearInterval(interval);
+document.getElementById('next').addEventListener('click', function () {
+    index = index === dataLength - 1 ? dataLength - 1 : index + 1;
     render();
 });
 
 function render() {
-    ReactDOM.render(course(), document.getElementById('root'));
+    data = courses_data[index];
+    ReactDOM.render(course(data), document.getElementById('root'));
 }
 
 render();
