b77f839d44be68e2acd71bda1b6d5fea444bd435
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-10-21 15:37:30 +0200

    Fixes & comments

--- scripts/app.jsx
+++ scripts/app.jsx
@@ -1,17 +1,23 @@
 var data = {
     title: 'Temat kursu',
     description: 'Opis kursu',
-    image: 'http://placehold.it/150x150'
+    image: 'http://placehold.it/150x150',
+    is_new: true,
+    is_promo: true
 };
 
 var course = (
     <div className="media">
+        {/* Course media column */}
         <div className="media-left">
             <img src={data.image} alt="cover" />
         </div>
+        {/* Course content column */}
         <div className="media-body">
-            <h3>{data.title}</h3>
+            <h3>{data.title} {data.is_new ? <span className="label label-default">Nowy!</span> : null}</h3>
             <p>{data.description}</p>
+            {/* Promotion */}
+            {data.is_promo ? <b>Kurs jest w Promocji!</b> : null}
         </div>
     </div>
 );
