ad4f108ea4965f172f32bb1441e1b34473fbc40d
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-10-22 12:32:03 +0200

    Component properties

--- scripts/app.jsx
+++ scripts/app.jsx
@@ -1,74 +1,62 @@
-var NewLabel = function (data) {
-    return data.is_new ? <span className="label label-default">Nowy!</span> : null;
-};
-var CoursePromoLabel = function (data) {
-    return data.is_promo ? <b>Kurs jest w Promocji!</b> : null;
-};
-var CourseActions = function (data) {
-    return (
-        <div className="btn-group pull-right">
-            <button className="btn btn-default">Szczegóły kursu</button>
-            <button className="btn btn-default">Dodaj do ulubionych kursów</button>
-            <button className="btn btn-default">Dodaj do koszyka</button>
-        </div>
-    );
-};
-var CourseDetails = function (data) {
-    return (
-        <table className="table course_details">
-            <tbody>
-                <tr>
-                    <th>Autor</th>
-                    <td>{data.author}</td>
-                </tr>
-                <tr>
-                    <th>Czas trwania</th>
-                    <td>{data.duration}</td>
-                </tr>
-            </tbody>
-        </table>
-    );
-};
-var CourseMedia = function (data) {
-    return <img src={data.image} alt="cover" />
-};
+var CourseMedia = ({ data }) => (<img src={data.image} alt="cover" />);
+var NewLabel = ({ data }) => (data.is_new ? <span className="label label-default">Nowy!</span> : null);
+var CoursePromoLabel = ({ data }) => (data.is_promo ? <b>Kurs jest w Promocji!</b> : null);
+var CourseActions = ({ data }) => (
+    <div className="btn-group pull-right">
+        <button className="btn btn-default">Szczegóły kursu</button>
+        <button className="btn btn-default">Dodaj do ulubionych kursów</button>
+        <button className="btn btn-default">Dodaj do koszyka</button>
+    </div>
+);
+var CourseDetails = ({ data }) => (
+    <table className="table course_details">
+        <tbody>
+            <tr>
+                <th>Autor</th>
+                <td>{data.author}</td>
+            </tr>
+            <tr>
+                <th>Czas trwania</th>
+                <td>{data.duration}</td>
+            </tr>
+        </tbody>
+    </table>
+);
 
-var Course = function (props) {
-    var data = props.data;
+var Course = (props) => {
+    var { data } = props;
     return (
         <div className="media">
             {/* Course media column */}
             <div className="media-left">
-                {CourseMedia(data)}
+                <CourseMedia {...props} />
             </div>
 
             {/* Course content column */}
             <div className="media-body">
-                <h3>{data.title} {NewLabel(data)}</h3>
+                <h3>{data.title} <NewLabel {...props} /></h3>
                 <p>{data.description}</p>
 
                 {/* Promotion */}
-                {CoursePromoLabel(data)}
+                <CoursePromoLabel {...props} />
 
                 {/* Course Actions */}
-                {CourseActions(data)}
+                <CourseActions {...props} />
             </div>
 
             {/* Course details collumn */}
             <div className="media-right">
-                {CourseDetails(data)}
+                <CourseDetails {...props} />
             </div>
         </div>
     );
 };
 
-var CoursesList = function (props) {
+var CoursesList = (props) => {
     var list = props.list;
     return (
         <div>
-            {list.map(function (data) {
-                return <Course data={data} key={data.id} />
-            })}
+            {list.map((data) => <Course data={data} key={data.id} />)}
         </div>
     );
 };
