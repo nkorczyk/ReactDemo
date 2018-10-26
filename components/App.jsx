11bcf9d873266d12713edb5af639689ca492dfe8
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-10-26 11:17:18 +0200

    Form element - checkbox

--- components/App.jsx
+++ components/App.jsx
@@ -91,6 +91,16 @@ const CourseForm = React.createClass({
             course: { ...this.state.course, author: event.target.value }
         });
     },
+    changedIsPromo: function (event) {
+        this.setState({
+            course: { ...this.state.course, is_promo: !this.state.course.is_promo }
+        })
+    },
+    changedIsNew: function (event) {
+        this.setState({
+            course: { ...this.state.course, is_new: !this.state.course.is_new }
+        })
+    },
     onSave: function (event) {
         event.preventDefault();
         this.props.onSave(this.state.course);
@@ -110,14 +120,33 @@ const CourseForm = React.createClass({
                         <textarea rows="10" type="text" className="form-control" value={this.state.course.description} onChange={this.changedDescription}></textarea>
                     </div>
                 </div>
-                <div className="form-group">
-                    <label className="control-label">Autor:</label>
-                    <div>
-                        <select className="form-control" value={this.state.course.author} onChange={this.changedAuthor}>
-                            {Object.keys(AppState.state.authors_map).map(author => 
-                                <option key={author} value={author}>{author}</option>
-                            )}
-                        </select>
+                <div className="row">
+                    <div className="col-xs-6">
+                        <div className="form-group">
+                            <div>
+                                <label className="control-label">Opcje:</label>
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label col-xs-6">
+                                    <input type="checkbox" checked={this.state.course.is_promo || ''} onChange={this.changedIsPromo} /> W promocji
+                                </label>
+                                <label className="control-label col-xs-6">
+                                    <input type="checkbox" checked={this.state.course.is_new || ''} onChange={this.changedIsNew} /> Nowość
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="form-group">
+                            <label className="control-label">Autor:</label>
+                            <div>
+                                <select className="form-control" value={this.state.course.author} onChange={this.changedAuthor}>
+                                    {Object.keys(AppState.state.authors_map).map(author =>
+                                        <option key={author} value={author}>{author}</option>
+                                    )}
+                                </select>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="form-group">
